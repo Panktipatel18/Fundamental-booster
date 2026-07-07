c7a24006c0071e1e222338765513051ac9ed2c1a
Panktipatel18 <panktipatel187@gmail.com>
2026-07-07 09:09:38 -0700

    Add files via upload

---
+++ PR.1 FUNDAMENTAL BOOSTER.py
@@ -0,0 +1,34 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================== RESTART: D:\PR.1 FUNDAMENTAL BOOSTER.py ===================
+WELCOME TO THE INTERACTIVE PERSONAL DATA COLLECTOR 
+Enter Your Name: Pankti Patel
+Enter Your Age: 26
+Enter Your Height (in cm): 1.70
+Enter Your Favourite Number: 8
+NAME
+Value : Pankti Patel
+Type : <class 'str'>
+ID : 1726687578736
+
+Age
+Value : 26
+Type : <class 'int'>
+ID : 140721148577688
+
+Height
+Value : 1.7
+Type : <class 'float'>
+ID : 1726730061136
+
+Favourite Number
+Value : 8
+Type : <class 'int'>
+ID : 140721148577112
+
+/n
+Approximate Birth Year : 2000
+/n
+Thank you for using the Personal Data Collector
+Have A Great Day!
